26e1baeb3e54d40f4867a41c1d445a6e915ed4dd
mahdi chekini <mchekini@checkconsulting.fr>
2025-03-09 20:26:19 +0100

    init project

---
+++ src/main/java/fr/formationacademy/scpiinvestpluspartner/ScpiInvestPlusPartnerApplication.java
@@ -0,0 +1,13 @@
+package fr.formationacademy.scpiinvestpluspartner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScpiInvestPlusPartnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScpiInvestPlusPartnerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/formationacademy/scpiinvestpluspartner/ScpiInvestPlusPartnerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.formationacademy.scpiinvestpluspartner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScpiInvestPlusPartnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
