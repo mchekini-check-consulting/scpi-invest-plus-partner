d1c4a63cd77c71bfaf901d60696d986fd665a2b0
speedkillsx <amayaslabchri88@gmail.com>
2025-03-18 18:40:06 +0100

    SCRUM-23: Use one broker in kafka

--- src/main/java/fr/formationacademy/scpiinvestpluspartner/configuration/kafkaConfig/KafkaTopicConfig.java
+++ src/main/java/fr/formationacademy/scpiinvestpluspartner/configuration/kafkaConfig/KafkaTopicConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
+import static fr.formationacademy.scpiinvestpluspartner.utils.Constants.SCPI_PARTNER_RESPONSE_TOPIC;
 import static fr.formationacademy.scpiinvestpluspartner.utils.Constants.SCPI_REQUEST_TOPIC;
 
 @Configuration
@@ -12,8 +13,16 @@ public class KafkaTopicConfig {
     @Bean
     public NewTopic getTopic() {
         return TopicBuilder.name(SCPI_REQUEST_TOPIC)
-                .partitions(1)
-                .replicas(1)
+                .partitions(3)
+                .replicas(3)
+                .build();
+    }
+
+    @Bean
+    public NewTopic getResponseTopic() {
+        return TopicBuilder.name(SCPI_PARTNER_RESPONSE_TOPIC)
+                .partitions(3)
+                .replicas(3)
                 .build();
     }
 }
