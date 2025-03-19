24f17ff5e28690f341f9e0f966aeb09a53f74dd6
AitAhmedNasser <aitahmednasser@outlook.com>
2025-03-19 22:58:20 +0100

    wip

---
+++ src/main/java/fr/formationacademy/scpiinvestpluspartner/entity/Investment.java
@@ -0,0 +1,7 @@
+package fr.formationacademy.scpiinvestpluspartner.entity;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "Investment")
+public class Investment {
+}
